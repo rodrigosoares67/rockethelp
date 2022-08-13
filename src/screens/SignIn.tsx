de2ea9f36fb6dbfb2de3a144436fc69f2ad17149
Rodrigo de Oliveira Carvalho Soares <rodrigo.soares@expocaccer.com.br>
2022-08-13 11:38:07 -0300

    FEAT: Criação dos estados para Name e Password

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { Heading, Text, VStack, Icon, useTheme } from "native-base";
 import { Envelope, Key } from 'phosphor-react-native';
 import Logo from '../assets/logo_primary.svg';
@@ -5,8 +6,14 @@ import { Button } from "../components/Button";
 import { Input } from "../components/Input";
 
 export function SignIn () {
+  const [name, setName] = useState('')
+  const [password, setPassword] = useState('')
 
   const { colors } = useTheme();
+
+  function handleSignIn(){
+
+  }
 
   return (
     <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
@@ -20,6 +27,7 @@ export function SignIn () {
         placeholder="E-mail" 
         mb={4}
         InputLeftElement={<Icon as={<Envelope color={colors.gray[300]} />} ml={4} />}
+        onChangeText={setName}
       />
 
       <Input 
@@ -27,9 +35,10 @@ export function SignIn () {
         InputLeftElement={<Icon as={<Key color={colors.gray[300]} />} ml={4} />}
         secureTextEntry
         mb={8}
+        onChangeText={setPassword}
       />
 
-      <Button title="Entrar" w="full" />
+      <Button title="Entrar" w="full" onPress={handleSignIn} />
     </VStack>
   )
 }
