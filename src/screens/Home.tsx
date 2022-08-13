428bfeaddbf2e6681181ae5fec403eb72f29b8b6
Rodrigo de Oliveira Carvalho Soares <rodrigo.soares@expocaccer.com.br>
2022-08-13 11:53:06 -0300

    FEAT: Criação da tela Home

---
+++ src/screens/Home.tsx
@@ -0,0 +1,29 @@
+import { HStack, IconButton, VStack, useTheme, Text } from 'native-base';
+import { SignOut } from 'phosphor-react-native';
+import Logo from '../assets/logo_secondary.svg';
+
+export function Home() {
+  const { colors } = useTheme();
+
+  return (
+    <VStack flex={1} pb={6} bg="gray.700">
+      <HStack
+        w="full"
+        justifyContent="space-between"
+        alignItems="center"
+        bg="gray.600"
+        pt={12}
+        pb={5}
+        px={6}
+      >
+        <Logo />
+
+        <IconButton
+          icon={<SignOut size={26} color={colors.gray[300]} />}
+        />
+      </HStack>
+
+      <Text></Text>
+    </VStack>
+  );
+}
